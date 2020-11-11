d683fa370430e915cfc0db49411d99759da5e8b0
Perkins Jon Ong <official.pjo@outlook.com>
2020-11-11 10:05:50 +0800

    bumped version

--- src/ALGame.cs
+++ src/ALGame.cs
@@ -16,7 +16,7 @@ namespace ADALotto.ClientLib
         private ADALottoClient ADALottoClient { get; set; }
         private const long HARD_CHECKPOINT = 4934993;
         private const long BLOCK_CRAWL_COUNT = 70;
-        public static string Version => "0.1.2-alpha";
+        public static string Version => "0.1.3-alpha";
         private bool IsSyncing { get; set; } = false;
         public bool IsInitialSyncFinished { get; set; } = false;
         public bool IsGameRunning => GameState?.GameGenesisTx != null;
